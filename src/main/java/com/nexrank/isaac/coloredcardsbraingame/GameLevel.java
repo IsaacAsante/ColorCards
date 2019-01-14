070edcab0081ef47b1f595a1532d515cdedd1250
Isaac Asante <isaac.editor16@gmail.com>
2019-01-15 06:07:27 +0800

    New GameLevel class with getInstance method added.

---
+++ src/main/java/com/nexrank/isaac/coloredcardsbraingame/GameLevel.java
@@ -0,0 +1,29 @@
+package com.nexrank.isaac.coloredcardsbraingame;
+
+public class GameLevel {
+
+    private String levelName;
+    private String levelNickname;
+    private int levelNo;
+
+    public GameLevel(String levelName, String levelNickname, int levelNo) {
+        this.levelName = levelName;
+        this.levelNickname = levelNickname;
+        this.levelNo = levelNo;
+    }
+
+    public static GameLevel newInstance(int levelNumber) {
+        switch (levelNumber) {
+            case 2:
+                return new GameLevel("Level 2", "Amateur", 2);
+            case 3:
+                return new GameLevel("Level 3", "Quick Thinker", 3);
+            case 4:
+                return new GameLevel("Level 4", "Match Master", 4);
+            case 5:
+                return new GameLevel("Level 5", "Superstar", 5);
+            default:
+                return new GameLevel("Level 1", "Beginner", 1);
+        }
+    }
+}
